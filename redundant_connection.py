09bd4f08c9cf9848286d03ab8f684dbbfdbe07da
Anton Belov <anton.belov.al7@gmail.com>
2024-05-29 10:54:57 -0300

    redundant connection solved

--- redundant_connection.py
+++ redundant_connection.py
@@ -4,22 +4,39 @@ from typing import List
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
 
-        parents = [i for i in range(1, len(edges)+1)]
-        rank = [1] * len(edges)
+        parents = [i for i in range(len(edges)+1)]
+        rank = [1] * (len(edges) + 1)
 
 
-        for e in edges:
-            if parents[e[1]-1] == parents[e[0]-1]:
-                return e
+        def find(n):
+            p = parents[n]
+
+            while p != parents[p]:
+                p = parents[p]
+
+            return p
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
             
-            if rank[parents[e[0]-1]-1] >= rank[parents[e[1]-1]-1]:
-                parents[e[1]-1] = parents[e[0]-1]
-                rank[parents[e[0]-1]-1] += 1
+            if rank[p1] > rank[p2]:
+                parents[p2] = p1
+                rank[p1] += rank[p2]
+            
             else:
-                parents[e[0]-1] = parents[e[1]-1]
-                rank[parents[e[1]-1]-1] += 1
+                parents[p1] = p2
+                rank[p2] += rank[p1]
+            
+            return True
+        
+        
+        for (e1, e2) in edges:
+            if not union(e1, e2):
+                return [e1, e2]
 
-            print(parents)
 
 
 edges = [[3,4],[1,2],[2,4],[3,5],[2,5]]
